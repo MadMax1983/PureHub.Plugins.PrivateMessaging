afe592fb54a895eee1723631bc8704372e9aeebb
maciek.korzeniewski@gmail.com <maciek.korzeniewski@gmail.com>
2017-11-23 19:30:46 +0100

    Added plugin solution.

---
+++ src/PureHub.Plugins.PrivateMessaging.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PureHub.Plugins.PrivateMessaging.Tests")]
+[assembly: AssemblyCompany("MK Software Solutions")]
+[assembly: AssemblyProduct("PureHub.Plugins.PrivateMessaging.Tests")]
+[assembly: AssemblyCopyright("Copyright © MK Software Solutions 2017")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("11a440c8-2fa6-47b5-9158-efee25914355")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
