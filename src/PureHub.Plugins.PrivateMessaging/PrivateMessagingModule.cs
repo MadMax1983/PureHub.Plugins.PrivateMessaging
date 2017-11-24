bc8cc0b0cd65826a45f11d66dcea3f2c0d1ecb4a
maciek.korzeniewski@gmail.com <maciek.korzeniewski@gmail.com>
2017-11-24 19:57:08 +0100

    Added Ninject support.

---
+++ src/PureHub.Plugins.PrivateMessaging/PrivateMessagingModule.cs
@@ -0,0 +1,25 @@
+﻿using Ninject.Modules;
+using Ninject.Web.Common;
+using PureHub.CQRS.Messaging;
+using PureHub.Plugins.PrivateMessaging.CQRS.Messaging;
+
+namespace PureHub.Plugins.PrivateMessaging
+{
+    public sealed class PrivateMessagingModule
+        : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ICommandHandler<SaveNewMessage>>().To<SaveNewMessageHandler>().InRequestScope();
+            Bind<ICommandHandler<SaveDraftMessage>>().To<SaveDraftMessageHandler>().InRequestScope();
+
+            Bind<ICommandHandler<SendNewMessage>>().To<SendNewMessageHandler>().InRequestScope();
+            Bind<ICommandHandler<SendDraftMessage>>().To<SendDraftMessageHandler>().InRequestScope();
+
+            Bind<ICommandHandler<MarkMessageAsRead>>().To<MarkMessageAsReadHandler>().InRequestScope();
+            Bind<ICommandHandler<MarkMessageAsUnread>>().To<MarkMessageAsUnreadHandler>().InRequestScope();
+
+            Bind<ICommandHandler<DeleteMessage>>().To<DeleteMessageHandler>().InRequestScope();
+        }
+    }
+}
